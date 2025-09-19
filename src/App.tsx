a9257de5d53b06fa18b7df1e59cee54cf7b36df5
Herman <hresherman@gmail.com>
2025-09-19 14:00:27 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    // const myFirstSubscribe = (event:MouseEvent<HTMLButtonElement>) => {
+    //     console.log('2')
+    // }
+    //
+    // const mySecSubscribe = (event: MouseEvent<HTMLButtonElement>) => {
+    //     console.log('Ivan')
+    // }
+
+    const onClickHandler = (name:string) =>{
+        console.log(name)
+
+    }
+
+    // const foo1 = () => {
+    //     console.log(100200)
+    // }
+    //
+    // const foo2 = (num: number) => {
+    //     console.log(num)
+    // }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => {onClickHandler('sss')}}>YoutubeChanel-2</button>
+      <button onClick={() => {onClickHandler('Hi')}}>YoutubeChanel-2</button>
+        {/*<button onClick={foo1}>1</button>*/}
+        {/*<button onClick={() =>foo2(100200)}>2</button>*/}
     </div>
   );
 }
