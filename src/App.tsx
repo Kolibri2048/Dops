b897a4184d4dab098960f6a4c5498589839b5d2e
Herman <hresherman@gmail.com>
2025-09-19 14:28:16 +0300

    Dops useStateF

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {Button} from "./components/Button";
 
 function App() {
-    // const myFirstSubscribe = (event:MouseEvent<HTMLButtonElement>) => {
-    //     console.log('2')
-    // }
-    //
-    // const mySecSubscribe = (event: MouseEvent<HTMLButtonElement>) => {
-    //     console.log('Ivan')
-    // }
 
-    // const onClickHandler = (name:string) =>{
-    //     console.log(name)
-    //
-    // }
-
-    // const foo1 = () => {
-    //     console.log(100200)
-    // }
-    //
-    // const foo2 = (num: number) => {
-    //     console.log(num)
-    // }
-
-    const Button1foo =(subscriber:string, age: number) => {
-        console.log(subscriber, age)
+    let [a, setA] = useState(1);
+    // let a = 1
+    const onClickHandler =() => {
+        setA(++a)
+        console.log(a)
     }
 
-    const Button2foo =(subscriber:string, age: number) => {
-        console.log(subscriber, age)
+    const onClickHandlerZero =() => {
+        setA(0)
+        console.log(a)
     }
   return (
     <div className="App">
-      <Button  name={'MyYutubeChanel-1'} onClick={() => Button1foo('Ivan', 21)}/>
-      <Button  name={'MyYutubeChanel-2'} onClick={() =>Button2foo ('Vasia', 20)}/>
+        <h1>{a}</h1>
+        <button onClick={onClickHandler}>number</button>
+        <button onClick={onClickHandlerZero}>zero</button>
     </div>
   );
 }
